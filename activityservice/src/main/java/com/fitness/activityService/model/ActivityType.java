0c296d22ae2067685b1eef02fc5b79701046a033
Piyush Waghela <piyush.waghela15514@sakec.ac.in>
2025-04-13 16:15:25 +0530

    coded total 3 services and  made interservice communication with netflix eureka server and client

---
+++ activityservice/src/main/java/com/fitness/activityService/model/ActivityType.java
@@ -0,0 +1,17 @@
+package com.fitness.activityService.model;
+
+
+
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
+}
