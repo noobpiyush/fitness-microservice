9732157d5703654cfc2aea50e6e64338297b0d71
Piyush Waghela <piyush.waghela15514@sakec.ac.in>
2025-04-14 13:27:03 +0530

    Added rabbitmq in the project & build the base of ai service

--- activityservice/src/main/java/com/fitness/activityService/config/WebClientConfig.java
+++ activityservice/src/main/java/com/fitness/activityService/config/WebClientConfig.java
@@ -1,5 +1,4 @@
 package com.fitness.activityService.config;
-
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- activityservice/src/main/java/com/fitness/activityService/service/ActivityService.java
+++ activityservice/src/main/java/com/fitness/activityService/service/ActivityService.java
@@ -5,6 +5,9 @@ import com.fitness.activityService.dto.ActivityRequest;
 import com.fitness.activityService.dto.ActivityResponse;
 import com.fitness.activityService.model.Activity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -13,10 +16,18 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ActivityService {
 
     private final ActivityRepository activityRepository;
     private final UserValidationService userValidationService;
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
 
     public ActivityResponse trackActivity(ActivityRequest request) {
 
@@ -36,6 +47,14 @@ public class ActivityService {
                 .build();
 
         Activity savedActivity = activityRepository.save(activity);
+
+        //publish to RabbitMQ for AI processing
+
+        try {
+            rabbitTemplate.convertAndSend(exchange, routingKey, savedActivity);
+        } catch (Exception e) {
+            log.error("Failed to publish activity to RabbitMQ : ", e);
+        }
 
         return mapToResponse(savedActivity);
     }
